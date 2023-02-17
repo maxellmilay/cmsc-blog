3e3daf6c781640305dcaad49e40042c59bb7ef33
Maxell Milay <milaymaxell@gmail.com>
2023-02-17 18:18:24 +0800

    [Adjustment][Maxell] updated page titles

--- src/pages/[blogNumber].tsx
+++ src/pages/[blogNumber].tsx
@@ -2,6 +2,7 @@ import Layout from '@/components/Layout';
 import React from 'react';
 import { useRouter } from 'next/router';
 import blogs from '@/constants/blogs';
+import Head from 'next/head';
 
 export default function SingleBlog() {
   const router = useRouter();
@@ -11,11 +12,16 @@ export default function SingleBlog() {
   });
 
   return (
-    <Layout>
-      <div className="flex flex-col items-center py-10">
-        <h1 className="text-5xl mb-10">{currentBlog?.title}</h1>
-        <p>{currentBlog?.description}</p>
-      </div>
-    </Layout>
+    <>
+      <Head>
+        <title>{currentBlog?.title}</title>
+      </Head>
+      <Layout>
+        <div className="flex flex-col items-center py-10">
+          <h1 className="text-5xl mb-10">{currentBlog?.title}</h1>
+          <p>{currentBlog?.description}</p>
+        </div>
+      </Layout>
+    </>
   );
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 export default function about() {
   return (
-    <Layout>
-      <div className="flex flex-col items-center py-10">
-        <h1 className="text-5xl mb-10">About Us</h1>
-        <p>
-          We are Computer Science students from the University of the Philippines Cebu, and we
-          created this blog in compliance with our course.
-        </p>
-      </div>
-    </Layout>
+    <>
+      <Head>
+        <title>About Us</title>
+      </Head>
+      <Layout>
+        <div className="flex flex-col items-center py-10">
+          <h1 className="text-5xl mb-10">About Us</h1>
+          <p>
+            We are Computer Science students from the University of the Philippines Cebu, and we
+            created this blog in compliance with our course.
+          </p>
+        </div>
+      </Layout>
+    </>
   );
 }
