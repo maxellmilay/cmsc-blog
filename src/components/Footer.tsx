e4ddcca4e60347c8757f43f6f435bb99dc956da8
Maxell Milay <milaymaxell@gmail.com>
2023-02-18 22:48:15 +0800

    [Feature][Maxell] added subscriber input function

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Open_Sans } from '@next/font/google';
 import SubscribeCall from './SubscribeCall';
+import Link from 'next/link';
 
 const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' });
 
@@ -10,13 +11,19 @@ export default function Footer() {
   return (
     <div className="flex flex-col sm:flex-row w-screen items-center sm:items-start footer-subscription-call bg-blog-primary py-14 px-10">
       <div className={`flex flex-col font-open ${openSansFont} text-white`}>
-        <p className="text-xl font-bold text-center sm:text-start">CMSC Blogs</p>
-        <p className="font-bold text-xs mt-2 text-blog-secondary text-center sm:text-start">
-          Blogs
-        </p>
-        <p className="font-bold text-xs mt-2 text-blog-secondary text-center sm:text-start">
-          About Us
-        </p>
+        <Link href="/">
+          <p className="text-xl font-bold text-center sm:text-start">CMSC Blogs</p>
+        </Link>
+        <Link href="/">
+          <p className="font-bold text-xs mt-2 text-blog-secondary text-center sm:text-start">
+            Blogs
+          </p>
+        </Link>
+        <Link href="/about">
+          <p className="font-bold text-xs mt-2 text-blog-secondary text-center sm:text-start">
+            About Us
+          </p>
+        </Link>
       </div>
       <div className="flex flex-col font-product mt-7 sm:mt-0">
         <SubscribeCall
--- src/components/SubscribeCall.tsx
+++ src/components/SubscribeCall.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface PropsInterface {
   emailTextColor: string;
@@ -10,11 +10,20 @@ interface PropsInterface {
 
 export default function SubscribeCall(props: PropsInterface) {
   const { emailTextColor, emailColor, subscribeTextColor, subscribeColor, borderColor } = props;
+  const [subscriberEmail, setSubscriberEmail] = useState('');
+
+  const handleSubscriberInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubscriberEmail(e.target.value);
+  };
 
   return (
     <div className="flex relative w-fit">
       <div className={`flex ${emailColor} rounded-[50vh] font-product border ${borderColor}`}>
-        <p className={`text-xs ${emailTextColor} my-3 ml-5 mr-32`}>Enter your email</p>
+        <input
+          placeholder="Enter Your Email"
+          className={`text-xs ${emailColor} ${emailTextColor} placeholder:${emailTextColor} py-3 ml-5 mr-32 outline-none`}
+          onChange={(e) => handleSubscriberInputChange(e)}
+        />
       </div>
       <div
         className={`flex absolute right-0 justify-center items-center ${subscribeColor} h-full rounded-[50vh] py-3 px-7`}
