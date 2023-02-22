e0415b74e2cb30583a7c07fbe2c9cf99c29ca9fa
Maxell Milay <milaymaxell@gmail.com>
2023-02-22 09:54:04 +0800

    [Adjustment][Maxell] added guard clause in individual blog render

--- src/pages/blogs/[blogNumber].tsx
+++ src/pages/blogs/[blogNumber].tsx
@@ -23,40 +23,40 @@ export default function SingleBlog() {
   return (
     <>
       <Head>
-        <title>{currentBlog?.title}</title>
+        <title>{currentBlog ? currentBlog.title : 'Blog'}</title>
       </Head>
       <Layout>
-        <div className="flex flex-col w-[85vw] sm:w-[70vw] lg:w-[65vw] pt-10 pb-16">
-          <div className="flex text-blog-secondary text-xs font-product items-center">
-            <p>{currentBlog?.date}</p>
-            <div className="w-1 h-1 rounded-[50%] bg-blog-red ml-2" />
-            <p className="ml-2">{currentBlog?.type}</p>
-          </div>
-          <h1
-            className={`${openSansFont} font-sans text-blog-primary font-bold text-4xl sm:text-6xl mt-3`}
-          >
-            {currentBlog?.title}
-          </h1>
-          <div className="w-full relative h-56 sm:h-[35rem] mt-7">
-            {currentBlog !== undefined && (
-              <Image src={currentBlog.picURL} fill className="object-cover" alt="Blog Image" />
-            )}
-          </div>
-          <p className="font-product text-xs text-blog-secondary mt-1">
-            {currentBlog?.imageCaption}
-          </p>
-          <p className="font-product text-sm text-justify mt-7 whitespace-pre-wrap">
-            {currentBlog?.description}
-          </p>
-          <div className="flex justify-end mt-3">
-            <button
-              className="text-white font-product text-xs bg-blog-primary py-2 px-5 rounded-[50vh]"
-              onClick={handleBackOnClick}
+        {currentBlog && (
+          <div className="flex flex-col w-[85vw] sm:w-[70vw] lg:w-[65vw] pt-10 pb-16">
+            <div className="flex text-blog-secondary text-xs font-product items-center">
+              <p>{currentBlog.date}</p>
+              <div className="w-1 h-1 rounded-[50%] bg-blog-red ml-2" />
+              <p className="ml-2">{currentBlog.type}</p>
+            </div>
+            <h1
+              className={`${openSansFont} font-sans text-blog-primary font-bold text-4xl sm:text-6xl mt-3`}
             >
-              Back
-            </button>
+              {currentBlog.title}
+            </h1>
+            <div className="w-full relative h-56 sm:h-[35rem] mt-7">
+              <Image src={currentBlog.picURL} fill className="object-cover" alt="Blog Image" />
+            </div>
+            <p className="font-product text-xs text-blog-secondary mt-1">
+              {currentBlog.imageCaption}
+            </p>
+            <p className="font-product text-sm text-justify mt-7 whitespace-pre-wrap">
+              {currentBlog.description}
+            </p>
+            <div className="flex justify-end mt-3">
+              <button
+                className="text-white font-product text-xs bg-blog-primary py-2 px-5 rounded-[50vh]"
+                onClick={handleBackOnClick}
+              >
+                Back
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </Layout>
     </>
   );
