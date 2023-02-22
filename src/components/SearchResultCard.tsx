290bc51cf35e7593272a8943fc32a7e2ad2eed4a
Maxell Milay <milaymaxell@gmail.com>
2023-02-22 10:02:55 +0800

    [Feature][Maxell] added search results redirection

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BlogInterface } from '@/interface/BlogInterface';
 import { Open_Sans } from '@next/font/google';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' });
 const openSansFont = openSans.variable;
@@ -12,9 +13,17 @@ interface PropsInterface {
 
 export default function SearchResultCard(props: PropsInterface) {
   const { blog } = props;
+  const router = useRouter();
+
+  const handleSearchResultCardClick = () => {
+    router.push(`/blogs/${blog.id}`);
+  };
 
   return (
-    <div className="flex flex-col items-center md:flex-row w-full search-result-card p-5 mt-10">
+    <div
+      className="flex flex-col items-center md:flex-row w-full search-result-card p-5 mt-10"
+      onClick={handleSearchResultCardClick}
+    >
       <div className="flex relative w-full md:w-64 h-48 sm:h-72 md:h-full min-h-[11rem] min-w-[16rem]">
         <Image src={blog.picURL} alt="Search Result" fill className="object-cover" />
       </div>
