fe132db212ca48c2110ff55dbf26058326ad539e
Maxell Milay <milaymaxell@gmail.com>
2023-02-26 02:06:48 +0800

    [Adjustment][Maxell] protected add blog route

--- src/pages/admin/add.tsx
+++ src/pages/admin/add.tsx
@@ -3,27 +3,34 @@ import Layout from '@/components/Layout';
 import UploadStepTab from '@/components/UploadStepTab';
 import UploadInputBox from '@/components/UploadInputBox';
 import InputSection from '@/components/InputSection';
+import ProtectedRoute from '@/components/ProtectedRoute';
+import Head from 'next/head';
 
 export default function Add() {
   const [currentStep, setCurrentStep] = useState(1);
 
   return (
-    <Layout>
-      <div className="flex flex-col w-full">
-        <div className="flex border-b custom-justify-between">
-          <div className="flex border-b-2 border-black py-5 px-16">
-            <p className="font-bold">UPLOAD YOUR OWN BLOG</p>
+    <ProtectedRoute>
+      <Head>
+        <title>Add Blog</title>
+      </Head>
+      <Layout>
+        <div className="flex flex-col w-full">
+          <div className="flex border-b custom-justify-between">
+            <div className="flex border-b-2 border-black py-5 px-16">
+              <p className="font-bold">UPLOAD YOUR OWN BLOG</p>
+            </div>
+            <div className="flex mr-10">
+              <UploadStepTab stepNumber={1} text="Add Preview" currentStep={currentStep} />
+              <UploadStepTab stepNumber={2} text="Add Main Content" currentStep={currentStep} />
+              <UploadStepTab stepNumber={3} text="Finish" currentStep={currentStep} />
+            </div>
           </div>
-          <div className="flex mr-10">
-            <UploadStepTab stepNumber={1} text="Add Preview" currentStep={currentStep} />
-            <UploadStepTab stepNumber={2} text="Add Main Content" currentStep={currentStep} />
-            <UploadStepTab stepNumber={3} text="Finish" currentStep={currentStep} />
+          <div className="flex flex-col h-10 mx-14 mt-10">
+            <InputSection type="Blog Title" input={UploadInputBox} />
           </div>
         </div>
-        <div className="flex flex-col h-10 mx-14 mt-10">
-          <InputSection type="Blog Title" input={UploadInputBox} />
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+    </ProtectedRoute>
   );
 }
