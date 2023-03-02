b7018bb251f727a642154200bc975c9083605bbf
Maxell Milay <milaymaxell@gmail.com>
2023-03-02 21:19:17 +0800

    [UI][Maxell] added dynamic profile preview

--- src/components/ProfilePreview.tsx
+++ src/components/ProfilePreview.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Open_Sans } from '@next/font/google';
+import UserContext from '@/context/UserContext';
+import Image from 'next/image';
 
 const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' });
 const openSansFont = openSans.variable;
 
 export default function ProfilePreview() {
+  const { userContext: user } = useContext(UserContext);
+
   return (
     <div className="flex justify-end items-center">
       <p className={`text-blog-primary text-xs ${openSansFont} font-sans font-bold`}>
-        Maxell Milay
+        {user.displayName}
       </p>
-      <div className="flex bg-blog-primary h-8 w-8 rounded-[50vh] ml-3" />
+      <div className="flex h-8 w-8 rounded-[50vh] ml-3">
+        {user.photoURL !== null && (
+          <Image src={user.photoURL} alt="DP" fill className="object-cover" />
+        )}
+      </div>
     </div>
   );
 }
