9f8b286ce3a3034a32627c04c225c765434630d5
Maxell Milay <milaymaxell@gmail.com>
2023-03-02 21:41:35 +0800

    [Fix][Maxell] fixed nextjs image config issue

--- src/components/ProfilePreview.tsx
+++ src/components/ProfilePreview.tsx
@@ -8,15 +8,16 @@ const openSansFont = openSans.variable;
 
 export default function ProfilePreview() {
   const { userContext: user } = useContext(UserContext);
+  console.log(user.photoURL);
 
   return (
     <div className="flex justify-end items-center">
       <p className={`text-blog-primary text-xs ${openSansFont} font-sans font-bold`}>
         {user.displayName}
       </p>
-      <div className="flex relative h-8 w-8 rounded-full ml-3">
+      <div className="flex relative h-8 w-8 ml-3">
         {user.photoURL !== null && (
-          <Image src={user.photoURL} alt="DP" fill className="object-cover" />
+          <Image src={user.photoURL} alt="DP" fill className="object-cover rounded-[50vh]" />
         )}
       </div>
     </div>
