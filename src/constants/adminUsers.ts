61e29ae12aea8e2f9fbd831b9bba23b8873b36a8
Maxell Milay <milaymaxell@gmail.com>
2023-03-03 12:21:53 +0800

    [Test][Maxell] added testing

--- src/constants/adminUsers.ts
+++ src/constants/adminUsers.ts
@@ -2,6 +2,7 @@ const adminUsers = [
   { name: 'Maxell Milay', email: 'milaymaxell@gmail.com' },
   { name: 'Jed Edison Donaire', email: 'jed.donaire08@gmail.com' },
   { name: 'Levi Bacarra', email: 'bacarradanelevi@gmail.com' },
+  { name: 'Jed Edison Donaire', email: 'jjdonaire@up.edu.ph' },
 ];
 
 export default adminUsers;
