c1c429791ceccaee18df126a78d1a69883835efb
Maxell Milay <milaymaxell@gmail.com>
2023-03-04 19:15:24 +0800

    [Info][Maxell] added a blog picture

--- src/constants/adminUsers.ts
+++ src/constants/adminUsers.ts
@@ -2,8 +2,6 @@ const adminUsers = [
   { name: 'Maxell Milay', email: 'milaymaxell@gmail.com' },
   { name: 'Jed Edison Donaire', email: 'jed.donaire08@gmail.com' },
   { name: 'Levi Bacarra', email: 'bacarradanelevi@gmail.com' },
-  { name: 'Jed Edison Donaire', email: 'jjdonaire@up.edu.ph' },
-  { name: 'Louise Deiparine', email: 'lddeiparine@up.edu.ph' },
 ];
 
 export default adminUsers;
